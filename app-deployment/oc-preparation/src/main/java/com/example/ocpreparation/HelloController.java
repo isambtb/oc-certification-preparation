3a4a050234d65e1d09434f019f82c0d42174e7e6
isambtb <isambtb@gmail.com>
2020-07-18 12:38:03 +0200

    ch1

--- app-deployment/oc-preparation/src/main/java/com/example/ocpreparation/HelloController.java
+++ app-deployment/oc-preparation/src/main/java/com/example/ocpreparation/HelloController.java
@@ -1,5 +1,16 @@
 package com.example.ocpreparation;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
+
+  @GetMapping
+  public String  getGreeting(){
+    return "Hello World!";
+  }
 
 }
