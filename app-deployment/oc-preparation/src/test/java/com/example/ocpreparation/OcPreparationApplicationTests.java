252c334299f232848ce43e2bc323f4a480abda43
isambtb <isambtb@gmail.com>
2020-07-18 12:31:25 +0200

    ch1

---
+++ app-deployment/oc-preparation/src/test/java/com/example/ocpreparation/OcPreparationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ocpreparation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcPreparationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
